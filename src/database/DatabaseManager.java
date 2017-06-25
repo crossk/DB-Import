9f4e7be3b91a2c62396b5fd1d5a9bdf58c3b99dd
Christian <Christian@Caboose.multi.box>
2017-06-25 15:17:22 +0200

    - package 'database' added 	- class DatabaseManager added
    - first commit

---
+++ src/database/DatabaseManager.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+/**
+ *
+ * @author Christian
+ * This class manages connections to a MySQL database. You can connect to it and
+ * execute querys and receive the results.
+ */
+public class DatabaseManager {
+    
+    public DatabaseManager(String host, String user, String pw, String db) {
+        //The constructor is taking the necessary information to connect to the
+        //database
+    }
+    
+}
